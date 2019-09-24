cb3f691d3cb4f698d0537c1a3783f77665beda18
nagao <t.nagaoo@gmail.com>
2019-09-24 09:58:28 +0900

    インデックス

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,15 @@
  *  画面ロード時処理
  *  初期表示
  */
-$(function(){
-    //会員認証
-    basicAuth(LOGIN_ID, TOKEN);
+$(function () {
+    if (USER_DATA == null) {
+        window.location.href = APP_DOMAIN + "app/oauth/login.html";
+    } else {
+        var url = SCHOOL_DATA[0]["url"];
+        if (temp == "manager") {
+            window.location.href = "lesson/mcalendar.html?id=" + url + "";
+        } else {
+            window.location.href = "lesson/calendar.html?id=" + url + "";
+        }
+    }
 });
